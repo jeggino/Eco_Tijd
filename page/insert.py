e604cb4186429df42cae520d86e9640b2fe7e64c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-07 18:27:57 +0200

    Update insert.py

--- page/insert.py
+++ page/insert.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import datetime
+from datetime import datetime, timedelta, date
+from natsort import natsorted
+import altair as alt
 
+from supabase import create_client, Client
+from streamlit_option_menu import option_menu
+
+import random
+
+from streamlit_cookies_controller import CookieController
+import time
+
+
+controller = CookieController()
+
+
+def logOut():
+    if st.button("logOut",use_container_width=True):
+        controller.remove("name")
+        st.rerun()
+
+
+
+logOut()
