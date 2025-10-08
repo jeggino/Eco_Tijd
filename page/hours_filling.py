e5bcac46ba6ece1bf8cc3021aaf12f44a8635902
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-08 14:55:43 +0200

    Update hours_filling.py

--- page/hours_filling.py
+++ page/hours_filling.py
@@ -111,7 +111,6 @@ if project:
     level = st.selectbox("Niveau", level_options, index=None, disabled=False, label_visibility="visible", accept_new_options=False, width="stretch")
 
     if level:
-      st.write(df[(df['project']==project)&(df['opdracht']==opdracht)&(df['level']==level)]['n_hours'].values)
       if df[(df['project']==project)&(df['opdracht']==opdracht)&(df['level']==level)]['n_hours'].values == 0:
         aantal_uren = st.number_input('Aantal uren', min_value=0, max_value=None, value='min', step=1, disabled=False, label_visibility="visible", icon=None, width="stretch")
       else: 
