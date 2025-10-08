0aace68931ef89ccf776ef8f6e4473f655930f9a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-08 15:40:03 +0200

    Update insert.py

--- page/insert.py
+++ page/insert.py
@@ -109,6 +109,7 @@ elif selected == 'Data':
   df = supabase.table("ekotijd_projects").select("*").execute()
   df = pd.DataFrame(df.data)      
   df
+  st.write('puparuolo')
   
   # except:
   #   st.image('https://t4.ftcdn.net/jpg/04/72/65/73/360_F_472657366_6kV9ztFQ3OkIuBCkjjL8qPmqnuagktXU.jpg',width=450)    
