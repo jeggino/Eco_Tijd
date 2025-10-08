66fc071756bb81f90af9cd084af30e219ab28c7c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-08 16:16:18 +0200

    Update insert.py

--- page/insert.py
+++ page/insert.py
@@ -130,9 +130,8 @@ elif selected == 'Projecten':
   )
 
   if event.selection:
-    # delete_item(df.loc[event.selection['rows'],'id')
-    # st.write(df.loc[event.selection['rows'],'id')
-    st.write(event.selection['rows'][0])
+    delete_item(df.loc[event.selection['rows'][0],'id')
+
 
   
   # except:
