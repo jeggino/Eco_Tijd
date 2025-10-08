bae9d55b332126be323e9821d9faf65ebbbdf793
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-08 16:39:55 +0200

    Update insert.py

--- page/insert.py
+++ page/insert.py
@@ -112,7 +112,7 @@ elif selected == 'Projecten':
 
 
 
-  event = st.data_editor(
+  event = st.dataframe(
       df,
       column_order = ['project','opdracht','level','n_hours','hour_loon'],
       column_config={
