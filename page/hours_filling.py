39a913bd1c20662c7b81821b0c689b9adce4cdf4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-08 15:10:29 +0200

    Update hours_filling.py

--- page/hours_filling.py
+++ page/hours_filling.py
@@ -113,7 +113,7 @@ if project:
     if level:
       st.write(df[(df['project']==project)&(df['opdracht']==opdracht)&(df['level']==level)]['n_hours'].values[0])
       if df[(df['project']==project)&(df['opdracht']==opdracht)&(df['level']==level)]['n_hours'].values[0] == 0:
-        aantal_uren = st.number_input('Aantal uren', min_value=0, max_value=None, value='min', step=0.5, disabled=False, label_visibility="visible", icon=None, width="stretch")
+        aantal_uren = st.number_input('Aantal uren', min_value=0.0, max_value=None, value='min', step=0.5, disabled=False, label_visibility="visible", icon=None, width="stretch")
       else: 
         aantal_uren = df[(df['project']==project)&(df['opdracht']==opdracht)&(df['level']==level)]['n_hours'].values[0]
                 
