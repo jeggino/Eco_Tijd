d1629d4be549fbab0e1ed7cd7b7065d4ce048d85
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-09 11:32:55 +0200

    Update dashboard.py

--- page/dashboard.py
+++ page/dashboard.py
@@ -57,6 +57,35 @@ def get_data(waarnemer):
     df = df[(df['waarnemer']==waarnemer)]
     return df
 
+@st.dialog(" ")
+def delete_item(id):
+  if st.button("Let op! Klik hier als je dit wilt verwijderen",icon="🚨",type="primary",use_container_width=True):
+      response = (
+          supabase.table("ekotijd_hours")
+          .delete()
+          .eq("id", id)
+          .execute()
+          )
+      st.rerun()
+
+@st.dialog(" ")
+def update_item(id,project_id,opdracht_id,level_id,n_hours_id,hour_loon_id):
+
+  project  = st.text_input('Project', value=project_id, max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+  opdracht = st.text_input('Opdracht', value=opdracht_id, max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+  level = st.selectbox("Niveau", ['Starter','Medior','Senior'], index=['Starter','Medior','Senior'].index(level_id), disabled=False, label_visibility="visible", accept_new_options=False, width="stretch")  
+
+  if st.button("**Update**", type="primary",use_container_width=True):
+      data = {"project":project,"opdracht":opdracht,"level":level,"n_hours":n_hours,'hour_loon':hour_loon}
+      response = (
+          supabase.table("ekotijd_hours")
+          .update(data)
+          .eq("id", id)
+          .execute()
+          )
+      
+      st.rerun()
+
 # --- APP ---
 
 
@@ -64,5 +93,51 @@ def get_data(waarnemer):
 
 df = get_data(waarnemer)
 
-df
+selected = option_menu(None,["Formulier", 'Taken'], icons=['bi-pen', 'bi-card-checklist'],orientation="horizontal",)
+
+if selected == "Formulier":
+  # with st.form("my_form", clear_on_submit=True,border=True): 
+  #   project  = st.text_input('Project', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+  #   opdracht = st.text_input('Opdracht', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+  #   level = st.selectbox("Niveau", ['Starter','Medior','Senior'], index=None, disabled=False, label_visibility="visible", accept_new_options=False, width="stretch")  
+  #   n_hours = st.number_input('Totaal vaste uren', min_value=0, max_value=None, value='min', step=1, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")    
+  #   hour_loon = st.number_input('Uur loon', min_value=None, max_value=None, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None,  placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+    
+  #   if st.form_submit_button("**Gegevens opslaan**",use_container_width=True):    
+  #       insert_project(project,opdracht,level,n_hours,hour_loon)
+
+elif selected == 'Taken':
+  
+  event = st.dataframe(
+      df,
+      column_order = ['datum','project','opdracht','level','aantal_uren','opmerking'],
+      column_config={
+          "datum":"Datum",
+        "project": "Project",
+        "opdracht": "Opdracht",
+        "level": "Niveau",
+        "aantal_uren": st.column_config.NumberColumn("Uur loon", format="€ %d"),
+        "opmerking": "Opmerking",
+
+      },
+      hide_index=True,
+      key="data",
+      on_select="rerun",
+      selection_mode=["single-row"]
+  )
+
+  if len(event.selection['rows']) != 0:
+    if st.button("Taak bijwerken",use_container_width=True):
+      id = df.loc[event.selection['rows'][0],'id']
+      project_id = df.loc[event.selection['rows'][0],'project']
+      opdracht_id = df.loc[event.selection['rows'][0],'opdracht']
+      level_id = df.loc[event.selection['rows'][0],'level']
+      n_hours_id = df.loc[event.selection['rows'][0],'n_hours']
+      hour_loon_id = df.loc[event.selection['rows'][0],'hour_loon']
+
+      update_item(id,project_id,opdracht_id,level_id,n_hours_id,hour_loon_id)
+      
+      
+    if st.button(":red[**Taak verwijder**]",use_container_width=True):
+      delete_item(df.loc[event.selection['rows'][0],'id'])
 
