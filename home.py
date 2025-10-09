6d34d4397f70ec2e67bba21364c27558b103714e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-10-09 11:24:38 +0200

    Update home.py

--- home.py
+++ home.py
@@ -68,7 +68,7 @@ def logIn():
 page_1 = st.Page("page/hours_filling.py", title="Uren invullen",icon=":material/timer:" )
 page_2 = st.Page("page/dashboard.py", title="Dashboard",icon=":material/bar_chart:" )
 page_3 = st.Page("page/dashboard_editor.py", title="Dashboard (algemeen)",icon=":material/bar_chart:" )
-page_4 = st.Page("page/insert.py", title="Project uploaden",icon=":material/upload:" )
+page_4 = st.Page("page/insert.py", title="Talen uploaden",icon=":material/upload:" )
 
 
 
